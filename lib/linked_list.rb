609049153d36bef27eea480ab32c3dfc345d3f9c
Steven McVey <smcvey1210@gmail.com>
2023-03-05 21:46:08 -0800

    commit tree e87dd7420a39835c8ec1bf28e2f3e546a17dcd11

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require "pry"
 
 class LinkedList
   attr_accessor :head, :tail
@@ -9,6 +10,30 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    if head.nil? || tail.nil? || head.next_node.nil?
+      return
+    end
+   new_tail = @head
+   new_head = @tail
+   @head = new_head
+   @tail = new_tail
+   node = @tail
+
+   loop do
+    reverse_node(node)
+    node = node.prev_node
+    break if !node.next_node
+   end
+
+   reverse_node(node)
+  end
+
+  def reverse_node(node)
+
+    prev = node.next_node ? node.next_node : nil
+    nex = node.prev_node ? node.prev_node : nil
+
+    node.prev_node = prev
+    node.next_node = nex
   end
 end
